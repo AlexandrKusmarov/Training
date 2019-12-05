d551af10146cb5616031d741c98d19c5fa37a2e2
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2019-12-05 14:30:53 +0300

    Took out the logic from Main into new class.

--- 2_class/src/main/java/QuadraticEquation.java
+++ 2_class/src/main/java/QuadraticEquation.java
@@ -1,4 +1,5 @@
 package main.java;
 
 public interface QuadraticEquation {
+    void resolveAcq(float a, float b, float c);
 }
--- 2_class/src/main/java/QuadraticEquationImpl.java
+++ 2_class/src/main/java/QuadraticEquationImpl.java
@@ -1,4 +1,25 @@
 package main.java;
 
-public class QuadraticEquation {
+public class QuadraticEquationImpl implements QuadraticEquation {
+    @Override
+    public void resolveAcq(float a, float b, float c) {
+        float x1;
+        float x2;
+        float discriminant = find(a, b, c);
+
+        if (discriminant < 0) {
+            System.out.println("The equation doesn't has roots.");
+        } else if (discriminant == 0) {
+            x1 = (-b + (float) Math.sqrt(discriminant)) / 2 * a;
+            System.out.println("The equation has one root: " + x1);
+        } else {
+            x1 = (-b + (float) Math.sqrt(discriminant)) / 2 * a;
+            x2 = (-b - (float) Math.sqrt(discriminant)) / 2 * a;
+            System.out.printf("The equation has two roots: x1= %s, x2= %s", x1, x2);
+        }
+    }
+
+    private static float find(float a, float b, float c) {
+        return b * b - 4 * a * c;
+    }
 }
