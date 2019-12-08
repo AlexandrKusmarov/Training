9e2b97b4ca147d7b78160f65a7e18c4e1cababd4
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2019-12-08 15:17:35 +0300

    Created empty model of the transport company.

---
+++ 3_class/src/main/java/trancomp/MainStarter.java
@@ -0,0 +1,7 @@
+package main.java.trancomp.model;
+
+public class MainStarter {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3_class/src/main/java/trancomp/model/Transportation.java
@@ -0,0 +1,4 @@
+package main.java.trancomp.model;
+
+public class Transportation {
+}
