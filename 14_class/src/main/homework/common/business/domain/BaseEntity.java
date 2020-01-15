7b96b64224ae2c68c68107422ad7dd5edbb830e6
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2020-01-15 13:50:23 +0300

    Added serialization for entities

--- 14_class/src/main/homework/common/business/domain/BaseEntity.java
+++ 14_class/src/main/homework/common/business/domain/BaseEntity.java
@@ -1,6 +1,8 @@
 package main.homework.common.business.domain;
 
-public abstract class BaseEntity {
+import java.io.Serializable;
+
+public abstract class BaseEntity implements Serializable {
   protected Long id;
 
   public Long getId() {
