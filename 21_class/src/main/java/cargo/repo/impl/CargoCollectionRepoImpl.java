7f2134cc397ff32778b6eee512a4672f39234624
Alexandr Kusmarov <Kusmarov_a@mail.ru>
2020-02-04 23:40:27 +0300

    Homework_21_DB

---
+++ 21_class/src/main/java/cargo/repo/impl/CargoCollectionRepoImpl.java
@@ -0,0 +1,103 @@
+package cargo.repo.impl;
+
+
+import cargo.domain.Cargo;
+import cargo.search.CargoSearchCondition;
+import common.solutions.utils.CollectionUtils;
+import storage.IdGenerator;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+import static storage.Storage.cargoCollection;
+
+public class CargoCollectionRepoImpl extends CommonCargoRepo {
+
+    @Override
+    public Optional<Cargo> getByIdFetchingTransportations(long id) {
+        return findById(id);
+    }
+
+    @Override
+    public Cargo[] findByName(String name) {
+
+        return cargoCollection.stream()
+                .filter(cargo -> cargo.getName().equals(name))
+                .toArray(Cargo[]::new);
+
+//        for (Cargo carrier : cargoCollection) {
+//            if (Objects.equals(carrier.getName(), name)) {
+//                result.add(carrier);
+//            }
+//        }
+    }
+
+    @Override
+    public List<Cargo> search(CargoSearchCondition searchCondition) {
+        List<Cargo> cargos = getAll();
+
+        if (CollectionUtils.isNotEmpty(cargos)) {
+            if (searchCondition.needSorting()) {
+                Comparator<Cargo> cargoComparator = createCargoComparator(searchCondition);
+                cargos.sort(searchCondition.isAscOrdering() ? cargoComparator : cargoComparator.reversed());
+            }
+        }
+
+        return cargos;
+    }
+
+    @Override
+    public Optional<Cargo> findById(Long id) {
+        return Optional.of(cargoCollection.stream()
+                .filter(cargo -> id != null && id.equals(cargo.getId()))
+                .findFirst()).get();
+
+//        for (Cargo cargo : cargoCollection) {
+//            if (id != null && id.equals(cargo.getId())) {
+//                return Optional.of(cargo);
+//            }
+//        }
+//        return Optional.empty();
+    }
+
+    @Override
+    public void save(Cargo cargo) {
+        cargo.setId(IdGenerator.generateId());
+        cargoCollection.add(cargo);
+    }
+
+    @Override
+    public boolean upLocalDateTime(Cargo entity) {
+        return true;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        Iterator<Cargo> iter = cargoCollection.iterator();
+
+        boolean removed = false;
+        while (iter.hasNext()) {
+            if (id != null && id.equals(iter.next().getId())) {
+                iter.remove();
+                removed = true;
+                break;
+            }
+        }
+
+        return removed;
+    }
+
+    @Override
+    public List<Cargo> getAll() {
+        return cargoCollection;
+    }
+
+    @Override
+    public int countAll() {
+        return cargoCollection.size();
+    }
+
+
+}
